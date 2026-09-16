9a29a65063e2ad07a0de2702c2deacaf654c9bcf
HeTianDi16888 <tenchi16888@gmail.com>
2026-09-16 02:51:26 -0400

    Add requirements.txt and build.bat for Windows packaging

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Building Wartales Save Editor...
-pyinstaller --onefile --windowed --icon=icon.ico --name="Wartales Save Editor" main.py
+pyinstaller --onefile --windowed --name="Wartales Save Editor" main.py
 echo.
 echo Build complete! The executable is in the 'dist' folder.
 pause
